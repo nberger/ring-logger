3941a633080311a3c55eec466a2684e20d93add8
Paul Legato <pjlegato@builtonlogic.com>
2013-01-27 21:50:06 -0800

    Change to OneLog as the main logging library

--- project.clj
+++ project.clj
@@ -1,10 +1,7 @@
-(defproject ring.middleware.logger "0.3.1-SNAPSHOT"
+(defproject ring.middleware.logger "0.4.0-SNAPSHOT"
   :description "Ring middleware to log each request using Log4J."
   :plugins [[lein-swank "1.4.4"]]
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.slf4j/slf4j-log4j12 "1.6.4"]
-                 [log4j "1.2.16"]
-                 [org.clojure/tools.logging "0.2.3"]
-                 [clj-logging-config "1.9.6"]
+                 [onelog "0.4.2-SNAPSHOT"]
                  [org.clojars.pjlegato/clansi "1.3.0"]
                  ])
