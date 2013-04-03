a6424387164f12f170a0ba3747fe15ca74c8a1dd
Paul Legato <pjlegato@gmail.com>
2013-04-03 14:52:02 -0700

    Upgrade onelog

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "Ring middleware to log each request using Log4J."
   :plugins [[lein-swank "1.4.4"]]
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [onelog "0.4.2-SNAPSHOT"]
+                 [onelog "0.4.3"]
                  [org.clojars.pjlegato/clansi "1.3.0"]
                  ])
