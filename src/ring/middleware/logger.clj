7ff6ab5b65a96c6b2f98cb9fd73506540231c700
Paul Legato <pjlegato@builtonlogic.com>
2014-04-10 14:29:22 -0700

    Refactor; allow user to disable ID logging

--- src/ring/middleware/logger.clj
+++ src/ring/middleware/logger.clj
@@ -1,7 +1,8 @@
 (ns ring.middleware.logger
   "Ring middleware that logs information about each request to a given
   set of generic logging functions."
-  (require
+  (:require
+   [org.tobereplaced (mapply :refer [mapply])]
    [clojure.java.io]
    [clojure.tools.logging]
    [clansi.core :as ansi]))
@@ -32,33 +33,27 @@
 
 (defn- format-id [id]
   "Returns a standard colorized, printable representation of a request id."
-  (apply ansi/style (format "%04x" id) [:bright] (get-colorization id)))
+  (when id
+    (str "["
+         (apply ansi/style (format "%04x" id) [:bright] (get-colorization id))
+         "]")))
 
 
 (defn- pre-logger
-  [logger-fns id {:keys [request-method uri remote-addr query-string params] :as req}]
-  ((:info logger-fns) (str "[" id "] Starting " request-method " " uri (if query-string (str "?" query-string)) " for " remote-addr
+  [{:keys [info debug id] :as options}
+   {:keys [request-method uri remote-addr query-string params] :as req}]
+  (info (str id "Starting " request-method " " uri (if query-string (str "?" query-string)) " for " remote-addr
                            " - " (dissoc (:headers req) "authorization"))) ;; don't log username/password, if any
 
-  ((:debug logger-fns) (str "[" id "] Request details: " (select-keys req [:server-port :server-name :remote-addr :uri 
-                                                                           :query-string :scheme :request-method 
-                                                                           :conent-type :content-length :character-encoding])))
+  (debug (str id "Request details: " (select-keys req [:server-port :server-name :remote-addr :uri 
+                                                       :query-string :scheme :request-method 
+                                                       :conent-type :content-length :character-encoding])))
   (if params
-    ((:info logger-fns) (str "[" id "]  \\ - - - -  Params: " params))))
-
-
-(defn- colorless-pre-logger
-  "Like pre-logger, but doesn't log any ANSI color codes."
-  [id request]
-
-  (ansi/without-ansi (pre-logger id request)))
+    (info (str id "  \\ - - - -  Params: " params))))
 
 
 (defn- post-logger
   "Logs data about a finished request.
-
-Log messages will be colorized with ANSI escape codes. Use
-colorless-logger if you want plaintext.
 
 The id is randomly colorized according to its value, to make it easier
 to visually correlate request/response/exception log sets.
@@ -66,58 +61,52 @@ to visually correlate request/response/exception log sets.
 Sends all log messages at \"info\" level to the logging
 infrastructure, unless status is >= 500, in which case they are sent as errors."
 
-  [logger-fns id {:keys [request-method uri remote-addr query-string] :as req}  {:keys [status] :as resp}  totaltime]
+  [{:keys [error info id] :as options}
+   {:keys [request-method uri remote-addr query-string] :as req}  {:keys [status] :as resp}  
+   totaltime]
   (let [colortime (try (apply ansi/style
                               (str totaltime)
                               (cond
                                (>= totaltime 1500)  [:bright :red]
-                               (>= totaltime 1000) [:red]
-                               (>= totaltime 500) [:yellow]
+                               (>= totaltime 800)   [:red]
+                               (>= totaltime 500)   [:yellow]
                                :else :default))
                        (catch Exception e (or totaltime "??")))
         
         colorstatus (try (apply ansi/style
                                 (str status)
                                 (cond
-                                 (< status 300) [:default] 
+                                 (< status 300)  [:default] 
                                  (>= status 500) [:bright :red] 
                                  (>= status 400) [:red] 
-                                 :else [:yellow]))
+                                 :else           [:yellow]))
                          (catch Exception e (or status "???")))
         log-message (str
-                     "[" id "] "
+                     id
                      "Finished " request-method " " uri  (if query-string (str "?" query-string)) " for " remote-addr " in (" colortime " ms)"
                      " Status: " colorstatus
                      ) ]
 
     (if (and (number? status) (>= status 500))
-      ((:error logger-fns) log-message)
-      ((:info logger-fns)  log-message))))
+      (error log-message)
+      (info  log-message))))
 
-
-(defn- colorless-post-logger
-  [id request response totaltime]
-  "Like post-logger, but doesn't log any ANSI color codes."
-  (ansi/without-ansi (post-logger id request response totaltime)))
 
 
 (defn- exception-logger
-  [id logger-fns {:keys [request-method uri remote-addr] :as request} throwable totaltime]
-  ((:error logger-fns) (str "[" id "] " (ansi/style "Exception!" :bright :red)  " for " remote-addr " in (" totaltime " ms)"))
-  ((:error logger-fns) throwable (str "- End stacktrace for " id "-"))) ;; must include a second string argument to make c.t.logging recognize first arg as an exception)
+  [{:keys [id error] :as options}
+   {:keys [request-method uri remote-addr] :as request}
+   throwable totaltime]
+  (error (str id (ansi/style "Exception!" :bright :red)  " for " remote-addr " in (" totaltime " ms)"))
+  (error throwable (str "- End stacktrace for " id "-"))) ;; must include a second string argument to make c.t.logging recognize first arg as an exception)
 
-
-(defn- colorless-exception-logger
-  [id request throwable totaltime]
-  "Like pre-logger, but doesn't log any ANSI color codes."
-  (ansi/without-ansi (exception-logger id request throwable totaltime)))
 
 ;; To make it easy to find this inside the handler for your own
 ;; logging, this is a dynamic variable.
 (def ^:dynamic *request-id* nil)
 
 (defn- make-logger-middleware
-  [handler logger-fns pre-logger post-logger exception-logger]
+  [handler & {:keys [pre-logger post-logger exception-logger disable-id-logging] :as options}]
   "Adds logging for requests using the given logger functions.
 
 The convenience function (wrap-with-logger) calls this function with
@@ -140,57 +129,67 @@ arguments: the id, the request, the Throwable that was thrown, and the
 total time taken to that point. It re-throws the exception after
 logging it, so that other middleware has a chance to do something with
 it.
+
+You can disable putting IDs in log entries by setting
+the :disable-id-logging option to true.
 "
-;; originally based on
+;; Long ago, originally based on
 ;; https://gist.github.com/kognate/noir.incubator/blob/master/src/noir.incubator/middleware.clj
   (fn [request]
     (let [start (System/currentTimeMillis)]
       (binding [*request-id* (format-id (rand-int 0xffff))]
-        (try
-          (pre-logger logger-fns *request-id* request)
-          (let [response (handler request)
-                finish (System/currentTimeMillis)
-                total  (- finish start)]
-            (post-logger logger-fns *request-id* request response total)
-            response)
-          (catch Throwable t
-            (let [finish (System/currentTimeMillis)
+
+        (let [options  (if disable-id-logging
+                         options
+                         (assoc options :id *request-id*))]
+          (try
+            (pre-logger options
+                        request)
+
+            (let [response (handler request)
+                  finish (System/currentTimeMillis)
                   total  (- finish start)]
-              (exception-logger logger-fns *request-id* request t total))
-            (throw t)))))))
+
+              (post-logger options
+                           request
+                           response
+                           total)
+
+              response)
+
+            (catch Throwable t
+              (let [finish (System/currentTimeMillis)
+                    total  (- finish start)]
+                (exception-logger options
+                                  request
+                                  t
+                                  total))
+              (throw t))))))))
 
 
-(def ctl-logging-fns
+(def default-options
   "Default logging functions that use clojure.tools.logging."
   {:info  (fn [x] (clojure.tools.logging/info x))
    :debug (fn [x] (clojure.tools.logging/debug x))
    :error (fn [x] (clojure.tools.logging/error x))
    :warn  (fn [x] (clojure.tools.logging/warn x))
+   :pre-logger pre-logger
+   :post-logger post-logger
+   :exception-logger exception-logger
    })
 
 
 (defn wrap-with-logger
   "Returns a Ring middleware handler which uses the prepackaged color loggers.
 
-   logger-fns is a map that must include the :info, :debug, and :error keys. 
+   Options may include the :info, :debug, and :error keys.
    Values are functions that accept a string argument and log it at that level.
-   If it is not given, uses the ones from clojure.tools.logging."
-  ([handler logger-fns]
-     (make-logger-middleware handler logger-fns pre-logger post-logger exception-logger))
+   Uses the ones from clojure.tools.logging if none are supplied."
+  ([handler & {:as options}]
+     (mapply (partial make-logger-middleware handler)
+                             (merge default-options
+                                    options))))
 
-  ([handler] (wrap-with-logger handler ctl-logging-fns)))
-
-
-(defn wrap-with-plaintext-logger
-  "Returns a Ring middleware handler which uses the ANSI-colorless prepackaged loggers.
-
-   suppress-log-initialization prevents the creation of superfluous stub
-   logfiles in environments where some other code has already
-   initialized the logger, such as in an Immutant container."
-  ([handler logger-fns]
-     (make-logger-middleware handler logger-fns colorless-pre-logger colorless-post-logger colorless-exception-logger))
-
-  ([handler] (wrap-with-plaintext-logger handler ctl-logging-fns)))
 
 
 (defn wrap-with-body-logger
