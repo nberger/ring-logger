2e586d425420f1b1dc16b26ea00779abb2c01444
Paul Legato <pjlegato@builtonlogic.com>
2014-04-10 01:39:16 -0700

    Bump to 0.5.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
-(defproject ring.middleware.logger "0.4.4-SNAPSHOT"
-  :description "Ring middleware to log each request using Log4J."
-  :plugins [[lein-swank "1.4.4"]]
+(defproject ring.middleware.logger "0.5.0-SNAPSHOT"
+  :description "Ring middleware to log each request."
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [onelog "0.4.3"]
                  [org.clojars.pjlegato/clansi "1.3.0"]
                  ])
