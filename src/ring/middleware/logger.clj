7ea8465b5fc3c1d209b4434b3db42c8ccdc59eb9
Paul Legato <pjlegato@builtonlogic.com>
2014-05-08 13:35:55 -0700

    Use log4j Nested Diagnostic Contexts for request ID rather than manually prepending it

--- src/ring/middleware/logger.clj
+++ src/ring/middleware/logger.clj
@@ -4,7 +4,8 @@
   (:require
    [org.tobereplaced (mapply :refer [mapply])]
    [clojure.java.io]
-   [clojure.tools.logging]
+   [onelog.core :as log]
+   [clj-logging-config.log4j :as log-config]
    [clansi.core :as ansi]))
 
 
@@ -33,38 +34,33 @@
 
 (defn- format-id [id]
   "Returns a standard colorized, printable representation of a request id."
-  (when id
-    (str "["
-         (apply ansi/style (format "%04x" id) [:bright] (get-colorization id))
-         "]")))
+  (if id
+    (apply ansi/style (format "%04x" id) [:bright] (get-colorization id))))
 
 
 (defn- pre-logger
-  [{:keys [info debug id] :as options}
+  [{:keys [info debug] :as options}
    {:keys [request-method uri remote-addr query-string params] :as req}]
-  (info (str id (ansi/style "Starting " :cyan)
+  (info (str (ansi/style "Starting " :cyan)
              request-method " " 
              uri (if query-string (str "?" query-string)) 
              " for " remote-addr
              " " (dissoc (:headers req) "authorization"))) ;; log headers, but don't log username/password, if any
 
-  (debug (str id "Request details: " (select-keys req [:server-port :server-name :remote-addr :uri 
+  (debug (str "Request details: " (select-keys req [:server-port :server-name :remote-addr :uri 
                                                        :query-string :scheme :request-method 
                                                        :conent-type :content-length :character-encoding])))
   (if params
-    (info (str id "  \\ - - - -  Params: " params))))
+    (info (str "  \\ - - - -  Params: " params))))
 
 
 (defn- post-logger
   "Logs data about a finished request.
 
-The id is randomly colorized according to its value, to make it easier
-to visually correlate request/response/exception log sets.
-
 Sends all log messages at \"info\" level to the logging
 infrastructure, unless status is >= 500, in which case they are sent as errors."
 
-  [{:keys [error info id] :as options}
+  [{:keys [error info] :as options}
    {:keys [request-method uri remote-addr query-string] :as req}  {:keys [status] :as resp}  
    totaltime]
   (let [colortime (try (apply ansi/style
@@ -84,15 +80,13 @@ infrastructure, unless status is >= 500, in which case they are sent as errors."
                                  (>= status 400) [:red] 
                                  :else           [:yellow]))
                          (catch Exception e (or status "???")))
-        log-message (str
-                     id
-                     (ansi/style "Finished " :cyan)
-                     request-method " " 
-                     uri  (if query-string (str "?" query-string))
-                     " for " remote-addr
-                     " in (" colortime " ms)"
-                     " Status: " colorstatus
-                     ) ]
+        log-message (str (ansi/style "Finished " :cyan)
+                         request-method " " 
+                         uri  (if query-string (str "?" query-string))
+                         " for " remote-addr
+                         " in (" colortime " ms)"
+                         " Status: " colorstatus
+                         ) ]
 
     (if (and (number? status) (>= status 500))
       (error log-message)
@@ -101,85 +95,75 @@ infrastructure, unless status is >= 500, in which case they are sent as errors."
 
 
 (defn- exception-logger
-  [{:keys [id error] :as options}
+  [{:keys [error] :as options}
    {:keys [request-method uri remote-addr] :as request}
    throwable totaltime]
-  (error (str id (ansi/style "Uncaught exception processing request:" :bright :red)  " for " remote-addr " in (" totaltime " ms) - request was: " request))
+  (error (str (ansi/style "Uncaught exception processing request:" :bright :red)  " for " remote-addr " in (" totaltime " ms) - request was: " request))
   (error throwable))
 
 
-;; To make it easy to find this inside the handler for your own
-;; logging, this is a dynamic variable.
-(def ^:dynamic *request-id* nil)
-
 (defn- make-logger-middleware
-  [handler & {:keys [pre-logger post-logger exception-logger disable-id-logging] :as options}]
+  [handler & {:keys [pre-logger post-logger exception-logger] :as options}]
   "Adds logging for requests using the given logger functions.
 
 The convenience function (wrap-with-logger) calls this function with
 default loggers. Use (make-logger) directly if you want to supply your own
 logger functions.
 
-Each request is assigned a random hex id, to allow the 3 possible
-logging events to be correlated.
+Each request is assigned a random hex id, to allow logged events
+relevant to a particular request to be correlated. This is exposed as
+a log4j Nested Diagnostic Context (NDC). Add a %x to your log format
+string to log it. (OneLog already includes the necessary %x by
+default.)
 
 The pre-logger function is called before the handler is invoked, and
-receives the id and the request as an argument.
+receives the request as an argument.
 
 The post-logger function is called after the response is generated,
-and receives four arguments: the id, the request, the response, and the
-total time taken by the handler.
+and receives the request, the response, and the total time taken by the handler as arguments
 
 The exception-logger function is called in a (catch) clause, if an
-exception is thrown during the handler's run. It receives four
-arguments: the id, the request, the Throwable that was thrown, and the
-total time taken to that point. It re-throws the exception after
-logging it, so that other middleware has a chance to do something with
-it.
-
-You can disable putting IDs in log entries by setting
-the :disable-id-logging option to true.
+exception is thrown during the handler's run. It receives the request,
+the Throwable that was thrown, and the total time taken to that
+point. It re-throws the exception after logging it, so that other
+middleware has a chance to do something with it.
 "
 ;; Long ago, originally based on
 ;; https://gist.github.com/kognate/noir.incubator/blob/master/src/noir.incubator/middleware.clj
   (fn [request]
     (let [start (System/currentTimeMillis)]
-      (binding [*request-id* (format-id (rand-int 0xffff))]
+      (log-config/with-logging-context (format-id (rand-int 0xffff))
+        (try
+          (pre-logger options
+                      request)
 
-        (let [options  (if disable-id-logging
-                         options
-                         (assoc options :id *request-id*))]
-          (try
-            (pre-logger options
-                        request)
+          (let [response (handler request)
+                finish (System/currentTimeMillis)
+                total  (- finish start)]
 
-            (let [response (handler request)
-                  finish (System/currentTimeMillis)
+            (post-logger options
+                         request
+                         response
+                         total)
+
+            response)
+
+          (catch Throwable t
+            (let [finish (System/currentTimeMillis)
                   total  (- finish start)]
-
-              (post-logger options
-                           request
-                           response
-                           total)
-
-              response)
-
-            (catch Throwable t
-              (let [finish (System/currentTimeMillis)
-                    total  (- finish start)]
-                (exception-logger options
-                                  request
-                                  t
-                                  total))
-              (throw t))))))))
+              (exception-logger options
+                                request
+                                t
+                                total))
+            (throw t)))))))
 
 
 (def default-options
-  "Default logging functions that use clojure.tools.logging."
-  {:info  (fn [x] (clojure.tools.logging/info x))
-   :debug (fn [x] (clojure.tools.logging/debug x))
-   :error (fn [x] (clojure.tools.logging/error x))
-   :warn  (fn [x] (clojure.tools.logging/warn x))
+  "Default logging functions."
+  {:info  (fn [x] (log/info x))
+   :debug (fn [x] (log/debug x))
+   :error (fn [x] (log/error x))
+   :warn  (fn [x] (log/warn x))
    :pre-logger pre-logger
    :post-logger post-logger
    :exception-logger exception-logger
@@ -191,7 +175,7 @@ the :disable-id-logging option to true.
 
    Options may include the :info, :debug, and :error keys.
    Values are functions that accept a string argument and log it at that level.
-   Uses the ones from clojure.tools.logging if none are supplied."
+   Uses OneLog to log if none are supplied."
   ([handler & {:as options}]
      (mapply (partial make-logger-middleware handler)
                              (merge default-options
@@ -205,9 +189,7 @@ the :disable-id-logging option to true.
   then putting them back into a new InputStream for other handlers to
   read. 
 
-  This is inefficient, and should only be used for debugging.
-
-  TODO: Add request ID."
+  This is inefficient, and should only be used for debugging."
   [handler logger-fns]
   (fn [request]
     (let [body (slurp (:body request))]
