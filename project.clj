8c262de2627ee095d19d53b11e5ccab38e2f242c
Paul Legato <pjlegato@builtonlogic.com>
2014-07-28 17:30:48 -0700

    Bump version

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojars.pjlegato/clansi "1.3.0"]
                  [org.tobereplaced/mapply "1.0.0"]
-                 [onelog "0.4.4-SNAPSHOT"]
+                 [onelog "0.4.5"]
                  ])
