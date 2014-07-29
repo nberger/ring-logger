7c906e43cd77f29a6dbdf06315f3f31d3dcc6851
Paul Legato <pjlegato@builtonlogic.com>
2014-07-29 15:37:36 -0700

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring.middleware.logger "0.5.0"
+(defproject ring.middleware.logger "0.5.1-SNAPSHOT"
   :description "Ring middleware to log each request."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojars.pjlegato/clansi "1.3.0"]
