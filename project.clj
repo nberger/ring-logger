fd50460126e973789404e3817d6369f266ac9fba
Paul Legato <pjlegato@builtonlogic.com>
2014-07-28 17:29:42 -0700

    Rel 0.5.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring.middleware.logger "0.5.0-SNAPSHOT"
+(defproject ring.middleware.logger "0.5.0"
   :description "Ring middleware to log each request."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojars.pjlegato/clansi "1.3.0"]
