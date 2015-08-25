d782561010d62e35ce8f1e4ae3174594e13675ee
Nicolas Berger <nicoberger@gmail.com>
2015-08-25 10:50:49 -0300

    tools.logging should be always installed as transitive dep
    It's still needed to load the ring.middleware.logger ns. But it's better
    than forcing a transitive dep on onelog, which brings many more transitive deps

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Ring middleware to log each request."
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojars.pjlegato/clansi "1.3.0"]
-                 [org.clojure/tools.logging "0.3.1" :scope "provided"]
+                 [org.clojure/tools.logging "0.3.1"]
                  [onelog "0.4.5" :scope "provided"]
                  [com.taoensso/timbre "4.1.1" :scope "provided"]]
   :profiles {:dev {:dependencies [[ring/ring-mock "0.2.0"]]}})
