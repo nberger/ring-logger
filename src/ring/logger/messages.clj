925da55f5c967a3901b2d363fea8dc1e769d94e8
Nicolas Berger <nicoberger@gmail.com>
2015-08-31 12:38:53 -0300

    Add :no-color printer implementations

--- src/ring/logger/messages.clj
+++ src/ring/logger/messages.clj
@@ -7,15 +7,23 @@
 
 (defmulti starting get-printer)
 
-(defmethod starting :default
-  [{:keys [info] :as options}
-   {:keys [request-method uri remote-addr query-string headers params] :as req}]
+(defn- make-default-starting-message
+  [options {:keys [request-method uri remote-addr query-string headers params] :as req}]
   (let [headers (dissoc headers "authorization")]
-    (info (str (ansi/style "Starting " :cyan)
-             request-method " "
-             uri (if query-string (str "?" query-string))
-             " for " remote-addr
-             " " headers))))
+    (str request-method " "
+         uri (if query-string (str "?" query-string))
+         " for " remote-addr
+         " " headers)))
+
+(defmethod starting :default
+  [{:keys [info] :as options} req]
+  (info (str (ansi/style "Starting " :cyan)
+             (make-default-starting-message options req))))
+
+(defmethod starting :no-color
+  [{:keys [info] :as options} req]
+  (info (str "Starting "
+             (make-default-starting-message options req))))
 
 (defmulti request-details get-printer)
 
@@ -47,34 +55,17 @@
 
 (defmulti finished get-printer)
 
-(defmethod finished :default
-  [{:keys [error info trace] :as options}
+(defn- make-and-log-finished-message
+  [{:keys [error info] :as options}
    {:keys [request-method uri remote-addr query-string] :as req}
    {:keys [status] :as resp}
-   totaltime]
-  (let [colortime (try (apply ansi/style
-                              (str totaltime)
-                              (cond
-                                (>= totaltime 1500)  [:bright :red]
-                                (>= totaltime 800)   [:red]
-                                (>= totaltime 500)   [:yellow]
-                                :else :default))
-                       (catch Exception e (or totaltime "??")))
-
-        colorstatus (try (apply ansi/style
-                                (str status)
-                                (cond
-                                  (< status 300)  [:default]
-                                  (>= status 500) [:bright :red]
-                                  (>= status 400) [:red]
-                                  :else           [:yellow]))
-                         (catch Exception e (or status "???")))
-        log-message (str (ansi/style "Finished " :cyan)
+   title time-str status-str]
+  (let [log-message (str title
                          request-method " "
                          uri  (if query-string (str "?" query-string))
                          " for " remote-addr
-                         " in (" colortime " ms)"
-                         " Status: " colorstatus
+                         " in (" time-str " ms)"
+                         " Status: " status-str
 
                          (when (= status 302)
                            (str " redirect to " (get-in resp [:headers "Location"]))))]
@@ -83,11 +74,53 @@
       (error log-message)
       (info  log-message))))
 
+(defmethod finished :default
+  [options req {:keys [status] :as resp} totaltime]
+  (let [time-str (try (apply ansi/style
+                              (str totaltime)
+                              (cond
+                                (>= totaltime 1500)  [:bright :red]
+                                (>= totaltime 800)   [:red]
+                                (>= totaltime 500)   [:yellow]
+                                :else :default))
+                       (catch Exception e (or totaltime "??")))
+
+        status-str (try (apply ansi/style
+                                (str status)
+                                (cond
+                                  (< status 300)  [:default]
+                                  (>= status 500) [:bright :red]
+                                  (>= status 400) [:red]
+                                  :else           [:yellow]))
+                         (catch Exception e (or status "???")))
+        title (ansi/style "Finished " :cyan)]
+    (make-and-log-finished-message options req resp title time-str status-str)))
+
+(defmethod finished :no-color
+  [options req {:keys [status] :as resp} totaltime]
+  (make-and-log-finished-message options
+                                 req
+                                 resp
+                                 "Finished "
+                                 (str totaltime)
+                                 (str status)))
+
 (defmulti exception get-printer)
 
 (defmethod exception :default
   [{:keys [error error-with-ex] :as options}
    {:keys [request-method uri remote-addr] :as request}
    throwable totaltime]
-  (error (str (ansi/style "Uncaught exception processing request:" :bright :red)  " for " remote-addr " in (" totaltime " ms) - request was: " request))
+  (error (str (ansi/style "Uncaught exception processing request:" :bright :red)
+              " for " remote-addr
+              " in (" totaltime " ms) - request was: " request))
+  (error-with-ex throwable ""))
+
+(defmethod exception :no-color
+  [{:keys [error error-with-ex] :as options}
+   {:keys [request-method uri remote-addr] :as request}
+   throwable totaltime]
+  (error (str "Uncaught exception processing request:"
+              " for " remote-addr
+              " in (" totaltime " ms) - request was: " request))
   (error-with-ex throwable ""))
