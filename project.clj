0316e3e7b67ceedbea7bd8b9643a43168a55d8c1
Nicolas Berger <nicoberger@gmail.com>
2015-09-02 13:31:07 -0300

    Bump to clojure 1.7.0

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :deploy-repositories [["releases" :clojars]]
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojars.pjlegato/clansi "1.3.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [com.taoensso/timbre "4.1.1" :scope "provided"]]
