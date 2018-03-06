b25096f986110781f2a0346d66f35ac0163d4a9e
Nicolas Berger <nicoberger@gmail.com>
2018-03-06 18:49:34 -0300

    Bump to clj 1.9.0 for 1.9 profile, use 1.9.0 in dev

--- project.clj
+++ project.clj
@@ -10,8 +10,9 @@
   :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
-             :1.9 {:dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]]}
-             :dev {:dependencies [[ring/ring-mock "0.2.0"]
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
+             :dev {:dependencies [[org.clojure/clojure "1.9.0"]
+                                  [ring/ring-mock "0.2.0"]
                                   [ring/ring-codec "1.0.0"]]}}
 
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
