90a319f02e0fec5b6b4666482ef973d3308ce155
Petr Mensik <petr.mensik1@gmail.com>
2018-03-08 15:22:12 +0100

    Add query string logging default settings test

--- test/ring/logger_test.clj
+++ test/ring/logger_test.clj
@@ -137,6 +137,19 @@
       (is (not (re-find #"secret_access_token" (-> entries last (nth 3)))))
       (is (not (re-find #"limit" (-> entries last (nth 3))))))))
 
+(deftest basic-ok-request-contains-query-string-by-default
+  (let [handler (-> (fn [req]
+                      {:status 200
+                       :body "ok" })
+                    (wrap-with-logger {:logger (make-test-logger)}))]
+    (handler (mock/request :get "/doc/10?limit=10&secret_access_token=****"))
+    (let [entries @*entries*]
+      (is (re-find #"secret_access_token" (-> entries first (nth 3))))
+      (is (re-find #"limit" (-> entries first (nth 3))))
+      (is (re-find #"query-string" (-> entries second (nth 3))))
+      (is (re-find #"secret_access_token" (-> entries last (nth 3))))
+      (is (re-find #"limit" (-> entries last (nth 3)))))))
+
 ;; async
 (deftest async-basic-ok-request-logging
   (let [handler (-> (fn [req respond raise]
