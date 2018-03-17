a70f315b538efcbdf171d70f00a109203fae837d
Nicolas Berger <nicoberger@gmail.com>
2018-03-17 20:04:22 -0300

    Add tests to check that it can log params and request independently

--- test/ring/logger_test.clj
+++ test/ring/logger_test.clj
@@ -13,6 +13,61 @@
      :body "ok"
      :headers {:ping "pong"}}))
 
+(deftest log-request-test
+  (let [output (atom [])
+        log (fn [& args]
+              (swap! output conj args))
+        handler (-> ok-handler
+                    wrap-params
+                    (logger/wrap-log-request {:log-fn log
+                                               :request-id-fn (constantly 42)}))
+        response (-> (mock/request :get
+                                   "/some/path?password=secret&email=foo@example.com")
+                     (handler))
+        [start finish :as lines] @output]
+    (is (= {:status 200
+            :body "ok"
+            :headers {:ping "pong"}}
+           response))
+    (is (= 2 (count lines)))
+    (is (= [:info nil {::logger/type :starting
+                       :request-method :get
+                       :uri "/some/path"
+                       ::logger/request-id 42}]
+           start))
+    (let [elapsed (-> finish last ::logger/ms)]
+      (is (= [:info nil {::logger/type :finish
+                         :request-method :get
+                         :uri "/some/path"
+                         :status 200
+                         ::logger/request-id 42
+                         ::logger/ms elapsed}]
+             finish))
+      (is (pos? elapsed)))))
+
+(deftest log-params-test
+  (let [output (atom [])
+        log (fn [& args]
+              (swap! output conj args))
+        handler (-> ok-handler
+                    (logger/wrap-log-params {:log-fn log})
+                    wrap-params)
+        response (-> (mock/request :get
+                                   "/some/path?password=secret&email=foo@example.com")
+                     (handler))
+        [params :as lines] @output]
+    (is (= {:status 200
+            :body "ok"
+            :headers {:ping "pong"}}
+           response))
+    (is (= 1 (count lines)))
+    (is (= [:debug nil {::logger/type :params
+                        :request-method :get
+                        :uri "/some/path"
+                        :params {"password" "secret"
+                                 "email" "foo@example.com"}}]
+           params))))
+
 (deftest log-request-log-params-test
   (let [output (atom [])
         log (fn [& args]
