c186ca326654a6c97fc7e4984963ecbd9506f1f0
Nicolas Berger <nicoberger@gmail.com>
2018-03-18 12:52:29 -0300

    Allow to transform the log message before it's passed to log-fn
    Introduces transform-fn which allows to filter/transform the log level &
    message. Adds a test using a transform-fn that outputs messages in a similar
    format as previous versions of ring.logger. This fn could be used for
    backwards compatibility
    
    Also adds :server-name to all messages

--- src/ring/logger.clj
+++ src/ring/logger.clj
@@ -7,59 +7,77 @@
 (defn default-request-id-fn [_]
   (rand-int 0xffffff))
 
-(defn default-log-fn [level throwable message]
+(defn default-log-fn [{:keys [level throwable message]}]
   (c.t.logging/log level throwable message))
+
+(defn make-transform-and-log-fn [transform-fn log-fn]
+  (fn [message]
+    (when-let [transformed (transform-fn message)]
+      (log-fn transformed))))
 
 (defn wrap-log-params
   ([handler] (wrap-log-params {}))
-  ([handler {:keys [log-fn log-level]
+  ([handler {:keys [log-fn log-level transform-fn]
              :or {log-fn default-log-fn
+                  transform-fn identity
                   log-level :debug}}]
    (fn [{:keys [::request-id] :as request}]
-     (log-fn log-level nil (-> (select-keys request [:request-method :uri])
-                                (assoc ::type :params
-                                     :params (:params request))
-                                (cond-> request-id (assoc ::request-id request-id))))
+     (let [log (make-transform-and-log-fn transform-fn log-fn)]
+       (log {:level log-level
+            :message (-> (select-keys request [:request-method :uri :server-name])
+                         (assoc ::type :params
+                                :params (:params request))
+                         (cond-> request-id (assoc ::request-id request-id)))}))
      (handler request))))
 
 (defn wrap-log-request
   ([handler] (wrap-log-request handler {}))
-  ([handler {:keys [request-id-fn log-fn log-exceptions?]
+  ([handler {:keys [request-id-fn log-fn log-exceptions? transform-fn]
              :or {request-id-fn default-request-id-fn
                   log-fn default-log-fn
+                  transform-fn identity
                   log-exceptions? true}}]
    (fn [request]
      (let [start-ms (System/currentTimeMillis)
-           request-id (request-id-fn request)]
-       (log-fn :info nil (-> (select-keys request [:request-method :uri])
-                             (assoc ::type :starting)
-                             (cond-> request-id (assoc ::request-id request-id))))
+           request-id (request-id-fn request)
+           log (make-transform-and-log-fn transform-fn log-fn)]
+       (log {:level :info
+             :message (-> (select-keys request [:request-method :uri :server-name])
+                          (assoc ::type :starting)
+                          (cond-> request-id (assoc ::request-id request-id)))})
        (try
          (let [response (-> request
                             (cond-> request-id (assoc ::request-id request-id))
                             (handler))
                elapsed-ms (- (System/currentTimeMillis) start-ms)]
-           (log-fn :info nil (-> (select-keys request [:request-method :uri])
-                                 (assoc ::type :finish
-                                        :status (:status response)
-                                        ::ms elapsed-ms)
-                                 (cond-> request-id (assoc ::request-id request-id))))
+           (log {:level :info
+                 :message (-> (select-keys request [:request-method :uri :server-name])
+                              (assoc ::type :finish
+                                     :status (:status response)
+                                     ::ms elapsed-ms)
+                              (cond-> request-id (assoc ::request-id request-id)))})
            response)
          (catch Exception e
            (when log-exceptions?
              (let [elapsed-ms (- (System/currentTimeMillis) start-ms)]
-               (log-fn :error e (-> (select-keys request [:request-method :uri])
-                                    (assoc ::type :exception
-                                           ::ms elapsed-ms)
-                                    (cond-> request-id (assoc ::request-id request-id))))))
+               (log {:level :error
+                     :throwable e
+                     :message (-> (select-keys request [:request-method :uri :server-name])
+                                  (assoc ::type :exception
+                                         ::ms elapsed-ms)
+                                  (cond-> request-id (assoc ::request-id request-id)))})))
            (throw e)))))))
 
 (defn wrap-with-logger
   "Returns a ring middleware handler that logs request and response details.
 
   Options may include:
-    * log-fn: used to do the actual logging. Accepts 3 params
+    * log-fn: used to do the actual logging. Accepts a map with keys
               [level throwable message]. Defaults to `clojure.tools.logging/log`.
+    * transform-fn: transforms a log item before it is passed through to log-fn. Messsage types
+              it might need to handle: [:start :params :finish :exception]. It can be
+              filter messages by returning nil. Log items are maps with keys:
+              [:level :throwable :message].
     * request-id-fn: takes a request and returns a unique request id which is logged
               and added to the request map under :ring.logger/request-id key.
               The key is not added when the fn returns nil. Defaults
--- test/ring/logger_test.clj
+++ test/ring/logger_test.clj
@@ -20,8 +20,8 @@
 
 (deftest log-request-test
   (let [output (atom [])
-        log (fn [& args]
-              (swap! output conj args))
+        log (fn [message]
+              (swap! output conj message))
         handler (-> ok-handler
                     wrap-params
                     (logger/wrap-log-request {:log-fn log
@@ -35,25 +35,29 @@
             :headers {:ping "pong"}}
            response))
     (is (= 2 (count lines)))
-    (is (= [:info nil {::logger/type :starting
-                       :request-method :get
-                       :uri "/some/path"
-                       ::logger/request-id 42}]
+    (is (= {:level :info
+            :message {::logger/type :starting
+                      :request-method :get
+                      :uri "/some/path"
+                      :server-name "localhost"
+                      ::logger/request-id 42}}
            start))
-    (let [elapsed (-> finish last ::logger/ms)]
-      (is (= [:info nil {::logger/type :finish
-                         :request-method :get
-                         :uri "/some/path"
-                         :status 200
-                         ::logger/request-id 42
-                         ::logger/ms elapsed}]
+    (let [elapsed (-> finish :message ::logger/ms)]
+      (is (= {:level :info
+              :message {::logger/type :finish
+                        :request-method :get
+                        :uri "/some/path"
+                        :server-name "localhost"
+                        :status 200
+                        ::logger/request-id 42
+                        ::logger/ms elapsed}}
              finish))
       (is (pos? elapsed)))))
 
 (deftest log-params-test
   (let [output (atom [])
-        log (fn [& args]
-              (swap! output conj args))
+        log (fn [message]
+              (swap! output conj message))
         handler (-> ok-handler
                     (logger/wrap-log-params {:log-fn log})
                     wrap-params)
@@ -66,17 +70,19 @@
             :headers {:ping "pong"}}
            response))
     (is (= 1 (count lines)))
-    (is (= [:debug nil {::logger/type :params
-                        :request-method :get
-                        :uri "/some/path"
-                        :params {"password" "secret"
-                                 "email" "foo@example.com"}}]
+    (is (= {:level :debug
+            :message {::logger/type :params
+                      :request-method :get
+                      :uri "/some/path"
+                      :server-name "localhost"
+                      :params {"password" "secret"
+                               "email" "foo@example.com"}}}
            params))))
 
 (deftest log-request-log-params-test
   (let [output (atom [])
-        log (fn [& args]
-              (swap! output conj args))
+        log (fn [message]
+              (swap! output conj message))
         handler (-> ok-handler
                     (logger/wrap-log-params {:log-fn log})
                     wrap-params
@@ -91,32 +97,38 @@
             :headers {:ping "pong"}}
            response))
     (is (= 3 (count lines)))
-    (is (= [:info nil {::logger/type :starting
-                       :request-method :get
-                       :uri "/some/path"
-                       ::logger/request-id 42}]
+    (is (= {:level :info
+            :message {::logger/type :starting
+                      :request-method :get
+                      :uri "/some/path"
+                      :server-name "localhost"
+                      ::logger/request-id 42}}
            start))
-    (is (= [:debug nil {::logger/type :params
-                        ::logger/request-id 42
+    (is (= {:level :debug
+            :message {::logger/type :params
+                      ::logger/request-id 42
+                      :request-method :get
+                      :uri "/some/path"
+                      :server-name "localhost"
+                      :params {"password" "secret"
+                               "email" "foo@example.com"}}}
+           params))
+    (let [elapsed (-> finish :message ::logger/ms)]
+      (is (= {:level :info
+              :message {::logger/type :finish
                         :request-method :get
                         :uri "/some/path"
-                        :params {"password" "secret"
-                                 "email" "foo@example.com"}}]
-           params))
-    (let [elapsed (-> finish last ::logger/ms)]
-      (is (= [:info nil {::logger/type :finish
-                         :request-method :get
-                         :uri "/some/path"
-                         :status 200
-                         ::logger/request-id 42
-                         ::logger/ms elapsed}]
+                        :server-name "localhost"
+                        :status 200
+                        ::logger/request-id 42
+                        ::logger/ms elapsed}}
              finish))
       (is (pos? elapsed)))))
 
 (deftest log-request-log-params-with-exception-test
   (let [output (atom [])
-        log (fn [& args]
-              (swap! output conj args))
+        log (fn [message]
+              (swap! output conj message))
         handler (-> throws-handler
                     (logger/wrap-log-params {:log-fn log})
                     wrap-params
@@ -130,31 +142,38 @@
         [start params logged-ex :as lines] @output]
     (is (= "Ooopsie" (.getMessage ex)))
     (is (= 3 (count lines)))
-    (is (= [:info nil {::logger/type :starting
-                       :request-method :get
-                       :uri "/some/path"
-                       ::logger/request-id 42}]
+    (is (= {:level :info
+            :message {::logger/type :starting
+                      :request-method :get
+                      :uri "/some/path"
+                      :server-name "localhost"
+                      ::logger/request-id 42}}
            start))
-    (is (= [:debug nil {::logger/type :params
-                        ::logger/request-id 42
+    (is (= {:level :debug
+            :message {::logger/type :params
+                      ::logger/request-id 42
+                      :request-method :get
+                      :uri "/some/path"
+                      :server-name "localhost"
+                      :params {"password" "secret"
+                               "email" "foo@example.com"}}}
+           params))
+    (let [elapsed (-> logged-ex :message ::logger/ms)]
+      (is (= {:level :error
+              :throwable ex
+              :message {::logger/type :exception
                         :request-method :get
                         :uri "/some/path"
-                        :params {"password" "secret"
-                                 "email" "foo@example.com"}}]
-           params))
-    (let [elapsed (-> logged-ex last ::logger/ms)]
-      (is (= [:error ex {::logger/type :exception
-                         :request-method :get
-                         :uri "/some/path"
-                         ::logger/request-id 42
-                         ::logger/ms elapsed}]
+                        :server-name "localhost"
+                        ::logger/request-id 42
+                        ::logger/ms elapsed}}
              logged-ex))
       (is (pos? elapsed)))))
 
 (deftest wrap-logger-test
   (let [output (atom [])
-        log (fn [& args]
-              (swap! output conj args))
+        log (fn [message]
+              (swap! output conj message))
         handler (-> ok-handler
                     (logger/wrap-with-logger {:log-fn log
                                               :request-id-fn (constantly 42)})
@@ -168,25 +187,31 @@
             :headers {:ping "pong"}}
            response))
     (is (= 3 (count lines)))
-    (is (= [:info nil {::logger/type :starting
-                       :request-method :get
-                       :uri "/some/path"
-                       ::logger/request-id 42}]
+    (is (= {:level :info
+            :message {::logger/type :starting
+                      :request-method :get
+                      :uri "/some/path"
+                      :server-name "localhost"
+                      ::logger/request-id 42}}
            start))
-    (is (= [:debug nil {::logger/type :params
-                        ::logger/request-id 42
+    (is (= {:level :debug
+            :message {::logger/type :params
+                      ::logger/request-id 42
+                      :request-method :get
+                      :uri "/some/path"
+                      :server-name "localhost"
+                      :params {"password" "secret"
+                               "email" "foo@example.com"}}}
+           params))
+    (let [elapsed (-> finish :message ::logger/ms)]
+      (is (= {:level :info
+              :message {::logger/type :finish
                         :request-method :get
                         :uri "/some/path"
-                        :params {"password" "secret"
-                                 "email" "foo@example.com"}}]
-           params))
-    (let [elapsed (-> finish last ::logger/ms)]
-      (is (= [:info nil {::logger/type :finish
-                         :request-method :get
-                         :uri "/some/path"
-                         :status 200
-                         ::logger/request-id 42
-                         ::logger/ms elapsed}]
+                        :server-name "localhost"
+                        :status 200
+                        ::logger/request-id 42
+                        ::logger/ms elapsed}}
              finish))
       (is (pos? elapsed)))))
 
@@ -210,12 +235,14 @@
     (is (= {::logger/type :starting
             :request-method :get
             :uri "/some/path"
+            :server-name "localhost"
             ::logger/request-id 42}
            start))
     (is (= {::logger/type :params
             ::logger/request-id 42
             :request-method :get
             :uri "/some/path"
+            :server-name "localhost"
             :params {"password" "secret"
                      "email" "foo@example.com"}}
            params))
@@ -223,8 +250,58 @@
       (is (= {::logger/type :finish
               :request-method :get
               :uri "/some/path"
+              :server-name "localhost"
               :status 200
               ::logger/request-id 42
               ::logger/ms elapsed}
              finish))
       (is (pos? elapsed)))))
+
+(deftest old-ring-logger-messages-output-fn-test
+  (let [output (atom [])
+        log (fn [message]
+              (swap! output conj message))
+        hostname "logger.example.com"
+        elapsed (atom nil)
+        transform-fn (fn [log-item]
+                       (update log-item
+                               :message
+                               (fn [{:keys [::logger/type request-method uri server-name
+                                            ::logger/ms status params]}]
+                                 (case type
+                                   :starting
+                                   (str "Starting " request-method " " uri " for " server-name)
+
+                                   :params
+                                   (str "  \\ - - - -  Params: " (pr-str params))
+
+                                   :finish
+                                   (do
+                                     (reset! elapsed ms)
+                                     (str "Finished " request-method " " uri " for " server-name
+                                          " in (" ms " ms)"
+                                          " Status: " status))))))
+        handler (-> ok-handler
+                    (logger/wrap-with-logger {:request-id-fn (constantly 42)
+                                              :transform-fn transform-fn
+                                              :log-fn log})
+                    wrap-params)
+        response (-> (mock/request :get
+                                   (str "http://" hostname "/some/path?password=secret&email=foo@example.com"))
+                     (handler))
+        [start params finish :as lines] @output]
+    (is (= 3 (count lines)))
+
+    (is (= {:level :info
+            :message "Starting :get /some/path for logger.example.com"}
+           start))
+
+    (is (= {:level :debug
+            :message "  \\ - - - -  Params: {\"password\" \"secret\", \"email\" \"foo@example.com\"}"}
+           params))
+
+    (is (= {:level :info
+            :message (str "Finished :get /some/path for logger.example.com in ("
+                          @elapsed " ms) Status: 200")}
+           finish))
+    (is (pos? @elapsed))))
