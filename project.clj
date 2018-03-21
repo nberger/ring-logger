27e8c0caaa304825bc366fbf5b48b846d112496f
Nicolas Berger <nicoberger@gmail.com>
2018-03-20 22:06:59 -0300

    Version 0.7.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-logger "0.7.8-SNAPSHOT"
+(defproject ring-logger "0.7.8"
   :description "Log ring requests & responses using your favorite logging backend."
   :url "https://github.com/nberger/ring-logger"
   :license {:name "Eclipse Public License"
